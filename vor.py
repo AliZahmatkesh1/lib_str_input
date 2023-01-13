bcc51813c00402b9bc2f83ae8277653f3069d9f3
alizahmatkesh <51386194+AliZahmatkesh1@users.noreply.github.com>
2023-01-13 07:35:23 +0330

    Update vor.py

--- vor.py
+++ vor.py
@@ -5,6 +5,7 @@ def chek(vr,vv=None):
     typ = str(type(vr))
     a = ["0","1","2","3","4","5","6","7","8","9"]
     a10 = ["a","b","c","d","e","f","j","h","i","g","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    akhar = "str_g"
     t = 1
     t1 = 1
     chek = 0
